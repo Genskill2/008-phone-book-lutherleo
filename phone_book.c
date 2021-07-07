91724ca8fe62b29fbe50393cab485ae2540db840
Love Kush Pranu <65358710+lutherleo@users.noreply.github.com>
2021-07-07 02:57:05 -0700

    Update phone_book.c

--- phone_book.c
+++ phone_book.c
@@ -44,7 +44,7 @@ int main(int argc, char *argv[]) {
   } 
 
   if (strcmp(argv[1], "add") == 0) {   /* Handle add */
-    if (argc != 4) {
+    if (argc !=4) {
       print_usage("Improper arguments for add", argv[0]);
       exit(1);
     }
@@ -62,8 +62,21 @@ int main(int argc, char *argv[]) {
     fclose(fp);
     exit(0);
   } else if (strcmp(argv[1], "search") == 0) {  /* Handle search */
-    printf("NOT IMPLEMENTED!\n"); /* TBD  */
-  } else if (strcmp(argv[1], "delete") == 0) {  /* Handle delete */
+  if(argc!=3){
+  print_usage("Improper arguments for search",argv[0]);
+    exit(1);
+    }
+  FILE *fp=open_db_file();
+  char *name=argv[2];
+  if (!(search(fp,name))){
+      printf("no match\n");
+      fclose(fp);
+      exit(1);
+    }
+  fclose(fp);
+  exit(0);
+    } /* TBD  */
+   else if (strcmp(argv[1], "delete") == 0) {  /* Handle delete */
     if (argc != 3) {
       print_usage("Improper arguments for delete", argv[0]);
       exit(1);
@@ -93,8 +106,14 @@ FILE *open_db_file() {
 }
   
 void free_entries(entry *p) {
+free(p);
+while(p->next!=NULL)
+{
+free(p->next);
+p=p->next;
+}
   /* TBD */
-  printf("Memory is not being freed. This needs to be fixed!\n");  
+  //printf("Memory is not being freed. This needs to be fixed!\n");  
 }
 
 void print_usage(char *message, char *progname) {
@@ -131,20 +150,17 @@ entry *load_entries(FILE *fp) {
   entry *tmp = NULL;
   /* Description of %20[^,\n]
      % is the start of the specifier (like %s, %i etc.)
-
      20 is the maximum number of characters that this will take. We
         know that names and phone numbers will be 20 bytes maximum so
         we limit it to that. %20s will read in 20 character strings
         (including the , to separate the name and phone number. That's
         why we use
-
     [^,\n] Square brackets are used to indicate a set of allowed
            characters [abc] means only a, b, or c. With the ^, it's
            used to specify a set of disallowed characters. So [^abc]
            means any character *except* a, b, or c. [^,] means any
            character except a , [^,\n] means any character except a
            comma(,) or a newline(\n).
-
     %20[^,\n] will match a string of characters with a maximum length
      of 20 characters that doesn't have a comma(,) or a newline(\n).
   */        
@@ -171,21 +187,23 @@ void write_all_entries(entry * p) {
 
 void add(char *name, char *phone) {
   FILE *fp = fopen(DB, "a");
-  fprintf(fp, "%s,%s\n", name, phone);
+  fprintf(fp,"%s,%s\n", name, phone);
   fclose(fp);
 }
 
 void list(FILE *db_file) {
   entry *p = load_entries(db_file);
   entry *base = p;
+  int count=0;
   while (p!=NULL) {
     printf("%-20s : %10s\n", p->name, p->phone);
     p=p->next;
-  }
+    count++;
+    }
+printf("Total entries :  %d\n",count);
   /* TBD print total count */
   free_entries(base);
 }
-
 
 int delete(FILE *db_file, char *name) {
   entry *p = load_entries(db_file);
@@ -195,6 +213,23 @@ int delete(FILE *db_file, char *name) {
   int deleted = 0;
   while (p!=NULL) {
     if (strcmp(p->name, name) == 0) {
+    if(prev!=NULL)
+    {
+    del=p;
+    prev->next=del->next;
+    free(del);
+    deleted=1;
+    }
+    else{
+    del=base;
+    base=base->next;
+    free(del);
+    deleted=1;
+    }
+    }
+    prev=p;
+    p=p->next;
+    
       /* Matching node found. Delete it from the linked list.
          Deletion from a linked list like this
    
@@ -207,9 +242,27 @@ int delete(FILE *db_file, char *name) {
       */
 
       /* TBD */
-    }
+    
   }
   write_all_entries(base);
   free_entries(base);
   return deleted;
+}
+
+int search(FILE *db_file, char *name){
+ entry *p = load_entries(db_file);
+  entry *base = p;
+  int m=0;
+  while (p!=NULL) {
+  if(strcmp(name,p->name)==0)
+   { printf("%s\n",p->phone);
+    m=1;}
+p=p->next;
+    }
+      if(m!=1)
+  {
+    return 0;
+  }
+  free_entries(base);
+  return 1;
 }
